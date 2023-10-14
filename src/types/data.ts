cbc64a70165b60f2858313cda41900e35ee5269b
darianuriarte <darianuriarte99@gmail.com>
2023-10-14 16:02:47 -0400

    Updated data model

--- src/types/data.ts
+++ src/types/data.ts
@@ -4,7 +4,15 @@ export type Datum = {
     price: number;
     dcf: number;
     name: string;
+    ticker: string;
+};
 
+/** Extracted stock information from Data */
+export type Stock = {
+    investmentRating: number;
+    ticker: string;
+    name: string;
+    data: Array<Datum>; // sorted by date
 };
 
 /** Processed data to be delivered to Frontend */
